0a04f936224ec6f825c1f6c0b9745c8035718c8f
William Easton <bill.easton@elastic.co>
2025-10-17 11:03:14 -0500

    fix bearspray type checking

--- key-value/key-value-shared/src/key_value/shared/type_checking/bear_spray.py
+++ key-value/key-value-shared/src/key_value/shared/type_checking/bear_spray.py
@@ -1,15 +1,16 @@
 from collections.abc import Callable
-from typing import Any
 
 from beartype import BeartypeConf, BeartypeStrategy, beartype
+from typing_extensions import ParamSpec, TypeVar
 
 no_bear_type_check_conf = BeartypeConf(strategy=BeartypeStrategy.O0)
 
 no_bear_type = beartype(conf=no_bear_type_check_conf)
 
+P = ParamSpec("P")
+R = TypeVar("R")
 
-def no_bear_type_check(func: Callable[..., Any]) -> Callable[..., Any]:
+def no_bear_type_check(func: Callable[P, R]) -> Callable[P, R]:
     return no_bear_type(func)
-
 
 bear_spray = no_bear_type_check
