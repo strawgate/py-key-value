a4a9d1d8ef86a6455736f173ea6ec52e39a29107
William Easton <bill.easton@elastic.co>
2025-10-26 00:04:27 -0500

    fix tests

--- key-value/key-value-shared/tests/utils/test_managed_entry.py
+++ key-value/key-value-shared/tests/utils/test_managed_entry.py
@@ -11,16 +11,19 @@ FIXED_DATETIME_STRING = FIXED_DATETIME.isoformat()
 
 @PositiveCases.parametrize(cases=SIMPLE_CASES)
 def test_dump_to_json(data: dict[str, Any], json: str, round_trip: dict[str, Any]):
+    """Test that the dump_to_json function dumps the data to the matching JSON string"""
     assert dump_to_json(data) == json
 
 
 @PositiveCases.parametrize(cases=SIMPLE_CASES)
 def test_load_from_json(data: dict[str, Any], json: str, round_trip: dict[str, Any]):
-    assert load_from_json(json) == data
+    """Test that the load_from_json function loads the data (round-trip) from the matching JSON string"""
+    assert load_from_json(json) == round_trip
 
 
 @PositiveCases.parametrize(cases=SIMPLE_CASES)
 def test_roundtrip_json(data: dict[str, Any], json: str, round_trip: dict[str, Any]):
+    """Test that the dump_to_json and load_from_json functions roundtrip the data"""
     dumped_json: str = dump_to_json(data)
     assert dumped_json == json
     assert load_from_json(dumped_json) == round_trip
